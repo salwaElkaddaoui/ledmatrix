b7ff139315941cef12c6bb7529ce95430ec5c3d5
SEKIsAMaker <elkaddaoui.salwa@gmail.com>
2021-03-23 20:27:16 +0100

    text scroll to the right - to fix

--- characters.h
+++ characters.h
@@ -1,10 +1,25 @@
-bool zero[3][5] = {{1, 1, 1, 1, 1}, {1, 0, 0, 0, 1}, {1, 1, 1, 1, 1}};
-bool one[3][5] = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {1, 1, 1, 1, 1}};
-bool two[3][5] = {{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 1, 1, 0, 1}};
-bool three[3][5] = {{1, 0, 1, 0, 1}, {1, 0, 1, 0, 1}, {1, 1, 1, 1, 1}};
-bool four[3][5] = {{1, 1, 1, 0, 0}, {0, 0, 1, 0, 0}, {1, 1, 1, 1, 1}};
-bool five[3][5] = {{1, 1, 1, 0, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 1}};
-bool six[3][5] = {{1, 1, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 1}};
-bool seven[3][5] = {{1, 0, 0, 0, 0}, {1, 0, 0, 0, 0}, {1, 1, 1, 1, 1}};
-bool eight[3][5] = {{1, 1, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 1, 1, 1, 1}};
-bool nine[3][5] = {{1, 1, 1, 0, 1}, {1, 0, 1, 0, 1}, {1, 1, 1, 1, 1}};
+//#define DEBUG 1
+
+#define MAT_H 8 // number of pixels
+#define MAT_W 8
+
+#define CHAR_H 5
+#define CHAR_W 3
+
+#define NUM_CHARS 10 //number of characters with a defined bitmap
+
+#define TEXT_SCROLL_SPEED 2000 // milliseconds
+#define MAX_TEXT_LENGTH 50 // number of characters in text string
+#define START_LINE 2 //index of matrix row where the top of the character lands
+
+bool zero[CHAR_W*CHAR_H] = {0, 0, 0, 0, 0,  0, 0, 0, 0, 0,  1, 1, 1, 1, 1};
+bool one[CHAR_W*CHAR_H]  = {0, 0, 0, 0, 0,  0, 0, 0, 0, 0,  1, 1, 1, 1, 1};
+bool two[CHAR_W*CHAR_H]  = {1, 0, 1, 1, 1,  1, 0, 1, 0, 1,  1, 1, 1, 0, 1};
+bool three[CHAR_W*CHAR_H] = {1, 0, 1, 0, 1,  1, 0, 1, 0, 1,  1, 1, 1, 1, 1};
+bool four[CHAR_W*CHAR_H] = {1, 1, 1, 0, 0,  0, 0, 1, 0, 0,  1, 1, 1, 1, 1};
+bool five[CHAR_W*CHAR_H] = {1, 1, 1, 0, 1,  1, 0, 1, 0, 1,  1, 0, 1, 1, 1};
+bool six[CHAR_W*CHAR_H] = {1, 1, 1, 1, 1,  1, 0, 1, 0, 1, 1, 0, 1, 1, 1};
+bool seven[CHAR_W*CHAR_H] = {1, 0, 0, 0, 0,  1, 0, 0, 0, 0,  1, 1, 1, 1, 1};
+bool eight[CHAR_W*CHAR_H] = {1, 1, 1, 1, 1,  1, 0, 1, 0, 1,  1, 1, 1, 1, 1};
+bool nine[CHAR_W*CHAR_H] = {1, 1, 1, 0, 1,  1, 0, 1, 0, 1,  1, 1, 1, 1, 1};
+bool* char_table[CHAR_W*CHAR_H] = { zero, one, two, three, four, five, six, seven, eight, nine};
--- ledmatrix.ino
+++ ledmatrix.ino
@@ -1,144 +1,215 @@
-/*
-MCP23O17 pin numbers:
-pin 0 is bit 0 of port A
-pin 7 is bit 7 of port A
-pin 8 is bit 0 of port B
-pin 15 is bit 7 of port B
-
-For an 8x8 ledmatrix where the pixel is 4cmx4cm large,
-use a frequency of 400k for the I2C bus, by changing the file:
-arduino-1.8.13/hardware/arduino/avr/libraries/Wire/src/utility/twi.h
-from #define TWI_FREQ 100000L to #define TWI_FREQ 400000L
-*/
-
 #include <Wire.h>
 #include <Adafruit_MCP23017.h>
+
+#include <string.h>
 #include "characters.h"
 
 Adafruit_MCP23017 mcp;
 
 void setup() {
+  #ifdef DEBUG
+    Serial.begin(9600);
+  #endif
+  
   mcp.begin();
-  for(int i=0; i<16; i++){
+  for(uint8_t i=0; i<16; i++)
     mcp.pinMode(i, OUTPUT);
-    }
-}
-
- /*  void set_column(uint8_t col_idx, bool pixels[]):
-   *
-   * Lights up a column of the led matrix
-   *
-   * uint8_t col_idx : the index of the column to light up
-   *
-   * bool pixels[]:  the states of the pixels of a column "col_idx"
-   * ordered from the top of the led matrix to its bottom
-
-   * Step 0: clear all LEDs
-   * Step 1: set the column "col_idx" and clear the rest of the columns:
-   * A column of the matrix connects the anodes of the leds.
-   * So setting a column consists of setting to HIGH
-   * the corresponding expander pin
-   * Step 2: put ON or OFF the leds of column col_idx depending
-   * on the values contained in input argument pixels[]
-   */
-void set_column_eachpin(uint8_t col_idx, bool pixels[]){
-
-  for(uint8_t i=0; i<8; i++)
-    mcp.digitalWrite(i, HIGH);
-
-  for(uint8_t i=8; i<16; i++) //Anodes are connected to Bank B of the MCP23O17
-    if(i!=col_idx+8)
-      mcp.digitalWrite(i, LOW);
-    else
-      mcp.digitalWrite(i, HIGH);
-
-  for(uint8_t i=0; i<8; i++) //Cathodes are connected to Bank A of the MCP23O17
-    if(pixels[i])
-      mcp.digitalWrite(i, LOW);
 }
 
 
-/*
-Set a column of the led matrix
-All banks of the GPIO expander are set in a single transmission
-*/
-void set_column(uint8_t col_idx, bool pixels[]){
+void set_column(uint8_t col_idx, bool matrix_col[]){
 
-  uint16_t ba = 0;
+  uint16_t ba = 0; //variable holding the data to be written in registers GPIOA and GPIOB
   byte BankA = 0xff; //Cathodes are connected to Bank A of the MCP23O17
   byte BankB = 0x00; //Anodes are connected to Bank B of the MCP23O17
-  //concatenate
   ba = (BankB << 8) | (BankA);
-  mcp.writeGPIOAB(ba); //to get rid of ghost lighting
+  mcp.writeGPIOAB(ba); //for ghost cancelling
 
   BankB |= 1 << col_idx;
   BankA = 0x00;
-  for(uint8_t i=0; i<8; i++)
-    if(pixels[i]==0)
+  for(uint8_t i=0; i<MAT_H; i++)
+    if(matrix_col[i]==0)
       BankA |= 1 << i;
   ba = (BankB << 8) | (BankA);
   mcp.writeGPIOAB(ba);
 }
 
-/*  void display_character(bool character[3][5]):
-  *
-  * Display character on led matrix by lighting up one column after another
-  *
-  * bool character[3][5] : the bitmap of the character to display
-  *
-  * for each column of the character's bitmap:
-  * Step 0: fill up the column of the ledmatrix accordingly
-  * Step 1: display the column
-  */
-void display_character(bool character[3][5]){
-  bool pixels[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-  for(int col=0; col<3; col++){
-    for(int i=0; i<5; i++){
-      pixels[i+3] = character[col][i];
-      }
-    set_column(7-col, pixels);
-    }
+void set_matrix(bool** matrix){
+  uint32_t time = millis();
+  while(millis() - time < TEXT_SCROLL_SPEED)
+    for(uint8_t i=0; i<MAT_W; i++)
+      set_column(i, matrix[i]);
 }
 
+void shift_to_left(bool** matrix, bool* newcol){
+  for(uint8_t i=0; i<MAT_W-1; i++)
+    memcpy(*(matrix+i), *(matrix+i+1), MAT_H * sizeof(bool));
+
+  memcpy(*(matrix+MAT_W-1), newcol, MAT_H * sizeof(bool));
+}
+
+void get_bitmap(char c, bool* bitmap){
+
+  uint8_t ascii_code = c;
+  int hash_code = -1;
+  if( ascii_code >= 48 && ascii_code <= 57 ) //0-9
+    hash_code = ascii_code - 48;
+  if(ascii_code >= 97 && ascii_code <= 122) //a-z
+    hash_code = ascii_code - 97+10;
+  
+  #ifdef DEBUG
+    Serial.print("ascii_code: "); Serial.println(ascii_code);
+    Serial.print("hash_code: "); Serial.println(hash_code);
+  #endif
+
+  if(hash_code >= 0)
+    for(uint8_t i=0; i<CHAR_W; i++)//for each column of the bitmap  
+      for(uint8_t j=0; j<CHAR_H; j++)//for each cell of the column
+        bitmap[ (CHAR_W-1-i)*MAT_H + j + START_LINE ] = char_table[hash_code][i*CHAR_H + j];
+  else{
+    #ifdef DEBUG
+      Serial.println("Unknown character in text.\nPlease reboot the system...");
+    #endif
+    while(true){}
+  }
+
+}
+
+void display_text(char* text){
+
+  bool** matrix = (bool**)calloc(MAT_W, sizeof(bool*));
+  for( int i=0; i<MAT_W; i++ ){
+    *(matrix+i) = (bool*)calloc(MAT_H, sizeof(bool));
+  }
+  
+  //num_filled_cols is used to keep track of the matrix columns
+  //that are filled with a displayable content. Once all columns are filled,
+  //we shift the matrix content to the left in order to make space for new content
+  uint8_t num_filled_cols = 0; 
+
+  //for each character of the text string
+  uint8_t c = 0;
+  while( text[c] != '\0' ){
+
+    //get its bitmap 
+    bool bitmap[CHAR_W*MAT_H] = {0};
+    get_bitmap( tolower( text[c] ), bitmap );
+  #ifdef DEBUG
+    Serial.println("inside display_text: ");
+    for(int  i=0; i<CHAR_W; i++){
+      for(int  j=0; j<MAT_H; j++){
+        Serial.print(bitmap[i*MAT_H+j]);
+        Serial.print(" ");
+      }
+      Serial.println("\n");
+    }
+    Serial.println("end of display_text");
+  #endif
+
+    //for each column of the bitmap of the character
+    for(uint8_t i=0; i<CHAR_W; i++){
+      
+      //if there's enough still unfilled columns on the matrix, copy it to the matrix 
+      if( num_filled_cols < MAT_W ){
+        memcpy( *(matrix+num_filled_cols), bitmap+i*MAT_H, MAT_H*sizeof(bool) );
+        num_filled_cols++;
+    #ifdef DEBUG
+        Serial.print("num_filled_cols: ");
+        Serial.println(num_filled_cols);
+    #endif
+      }
+      else{ //this means that all columns of the matrix are filled, 
+            //and we need to display them
+            //and make space for new content by shifting the led matrix
+        set_matrix(matrix);
+        shift_to_left(matrix, bitmap+i*MAT_H);
+      }
+    }
+
+  #ifdef DEBUG
+  Serial.println("inside display_text: ");
+    for(int  i=0; i<MAT_W; i++){
+      for(int  j=0; j<MAT_H; j++){
+        Serial.print(matrix[i][j]);
+        Serial.print(" ");
+      }
+      Serial.println("\n");
+    }
+  #endif
+  c++;
+  }
+
+  //keep scrolling until the text disappears in the left direction
+  bool blank[MAT_H] = {0};
+  while( num_filled_cols > 0 ){
+    set_matrix(matrix);
+    shift_to_left(matrix, blank);
+    num_filled_cols--;
+  }
+  for(int i=0; i<MAT_W; i++)
+    free(matrix[i]);
+  free(matrix);
+}
+
+int matrixState = 1;
+unsigned long previousMillis = millis();
+unsigned long interval = 1000;
+
 void loop() {
-  uint32_t time = millis();
-  while(millis() - time < 1000)
-    display_character(zero);
 
-  time = millis();
-  while(millis() - time < 1000)
-    display_character(one);
+  bool col_0[8] = {false, false, true, false, true, true, false, false};
+  bool col_1[8] = {false, true, true, false, false, true, true, false};
+  bool col_2[8] = {false, false, true, false, true, true, false, false};
+  bool col_3[8] = {false, false, false, false, false, true, false, false};
+  bool col_4[8] = {false, false, true, true, false, false, true, false};
+  bool col_5[8] = {false, true, true, false, true, true, true, false};
+  bool col_6[8] = {false, true, false, false, false, false, true, false};
+  bool col_7[8] = {false, false, true, false, true, true, false, false};
+  bool none[8] = {false, false, false, false, false, false, false, false};
+  bool* matrix[8] = { col_0, col_1, col_2, col_3, col_4, col_5, col_6, col_7 }; 
+  bool* nones[8] = { none, none, none, none, none, none, none, none };
 
-  time = millis();
-  while(millis() - time < 1000)
-    display_character(two);
+ /* 
+  set_matrix(matrix);
 
-  time = millis();
-  while(millis() - time < 1000)
-    display_character(three);
+  unsigned long currentMillis = millis();
 
-  time = millis();
-  while(millis() - time < 1000)
-    display_character(four);
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
 
-  time = millis();
-  while(millis() - time < 1000)
-    display_character(five);
+    // if the LED is off turn it on and vice-versa:
+    if (matrixState == 1) {
+      matrixState = 0;
+    } else {
+      matrixState = 0;
+    }
 
-  time = millis();
-  while(millis() - time < 1000)
-    display_character(six);
+    // set the LED with the ledState of the variable:
+    if( matrixState==0 )
+      set_matrix(nones);
+    else
+      set_matrix(matrix);
+  }
+  */
 
-  time = millis();
-  while(millis() - time < 1000)
-    display_character(seven);
+  static int counter = 0;
+ 
 
-  time = millis();
-  while(millis() - time < 1000)
-    display_character(eight);
-
-  time = millis();
-  while(millis() - time < 1000)
-    display_character(nine);
-
+  //char c = '1';
+  char text[4] = {'1', '2', '9', '\0'};
+  
+//  if(counter == 0){
+    display_text(text);
+  /*
+    get_bitmap(c, bitmap);
+    for(int  i=0; i<CHAR_W; i++){
+      for(int  j=0; j<MAT_H; j++){
+        Serial.print(bitmap[i*MAT_H+j]);
+        Serial.print(" ");
+      }
+      Serial.println("\n");
+    }*/
+//  counter++;
+//  }
 }
